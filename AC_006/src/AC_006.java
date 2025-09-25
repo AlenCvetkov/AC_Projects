e11de5f02a63ecbd42d693ba0bf0b223559d937b
Alen Cvetkov <alen.cvetkov@students.finki.ukim.mk>
2025-09-25 23:39:02 +0200

    Added AC_006

--- AC_006/src/AC_006.java
+++ AC_006/src/AC_006.java
@@ -1,22 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AC_006 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int n = sc.nextInt();  // number of statements
-        int x = 0;
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
 
+        int[] speeds = new int[n];
         for (int i = 0; i < n; i++) {
-            String statement = sc.next();
-            if (statement.contains("++")) {
-                x++;
-            } else {
-                x--;
-            }
+            speeds[i] = scanner.nextInt();
         }
 
-        System.out.println(x);
+        // Sort in ascending order
+        Arrays.sort(speeds);
+
+        // k-th largest from the end = index n-k
+        System.out.println(speeds[n - k]);
+
+        scanner.close();
     }
 }
